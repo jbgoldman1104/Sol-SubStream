a4a1185405973335eeb33f9fa27d8ac891210beb
Golden Winner <jbgoldman1104@gmail.com>
2024-06-04 16:04:47 +0000

    udpate dex_trade substreams

---
+++ dex-trades/src/pb/mod.rs
@@ -0,0 +1,14 @@
+// @generated
+pub mod sf {
+    pub mod solana {
+        pub mod dex {
+            pub mod trades {
+                // @@protoc_insertion_point(attribute:sf.solana.dex.trades.v1)
+                pub mod v1 {
+                    include!("sf.solana.dex.trades.v1.rs");
+                    // @@protoc_insertion_point(sf.solana.dex.trades.v1)
+                }
+            }
+        }
+    }
+}
