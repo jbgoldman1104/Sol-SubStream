379d5f93e6adcce7e8de3ec89539c9b610757615
Golden Winner <jbgoldman1104@gmail.com>
2024-06-10 18:55:49 +0000

    Updated for token info

--- substreams-sink-service/index.ts
+++ substreams-sink-service/index.ts
@@ -4,6 +4,49 @@ import { type ActionOptions } from "./bin/cli.js";
 
 import { initialize, insertTrades } from './postgres/trade.js'
 
+import { Metaplex } from "@metaplex-foundation/js";
+import { Connection, PublicKey } from "@solana/web3.js";
+import {
+    getMint,
+    getAccount,
+    getAssociatedTokenAddress,
+} from "@solana/spl-token";
+
+async function getTokenMetadata(token: String) {
+    try {
+        const connection = new Connection("https://mainnet.helius-rpc.com/?api-key=e4226aa3-24f7-43c1-869f-a1b1e3fbb148");
+        const metaplex = Metaplex.make(connection);
+
+        const mintAddress = new PublicKey(token);
+
+        let tokenName;
+        let tokenSymbol;
+        let tokenLogo;
+
+        const metadataAccount = metaplex
+            .nfts()
+            .pdas()
+            .metadata({ mint: mintAddress });
+
+        const metadataAccountInfo = await connection.getAccountInfo(metadataAccount);
+
+        if (metadataAccountInfo) {
+            const token = await metaplex.nfts().findByMint({ mintAddress: mintAddress });
+            tokenName = token.name;
+            tokenSymbol = token.symbol;
+            tokenLogo = token.json?.image;
+        }
+        const mintInfo = await getMint(connection, mintAddress);
+        console.log("tokenName: " + tokenName);
+        console.log("tokenSymbol: " + tokenSymbol);
+        console.log("tokenLogo: " + tokenLogo);
+        console.log("Decimals: " + mintInfo.decimals);
+        console.log("Supply: " + mintInfo.supply);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 if (false) {
     await initialize()
 }
@@ -15,3 +58,6 @@ export async function action(options: ActionOptions) {
     await emitter.start();
     http.server.close()
 }
+
+
+getTokenMetadata("C2AzGig8NveCJCPTjdXHs4WePudYGzgsz4LXsRQaZsBb").then(()=>{}).catch(error=>{});
